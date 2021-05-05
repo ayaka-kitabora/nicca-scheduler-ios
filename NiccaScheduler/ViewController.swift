288cea87701e1ad074ae978084c164e654cde993
Pocchi <pocchi13.tr@gmail.com>
2021-05-05 18:04:07 +0900

    Initial Commit

---
+++ NiccaScheduler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiccaScheduler
+//
+//  Created by 北洞亜也加 on 2021/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
