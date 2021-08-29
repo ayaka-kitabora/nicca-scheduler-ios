38007addef73e7ce49bd2a0395cbf44adafde945
Pocchi <pocchi13.tr@gmail.com>
2021-08-29 20:25:40 +0900

    sheduleEndDate datepicker

--- NiccaScheduler/controller/TaskInputViewController.swift
+++ NiccaScheduler/controller/TaskInputViewController.swift
@@ -13,13 +13,37 @@ class TaskInputViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var taskNameTextField: UITextField!
     @IBOutlet weak var pageAllCountTextField: UITextField!
     @IBOutlet weak var scheduleEndAtTextField: UITextField!
+
+    var datePicker: UIDatePicker = UIDatePicker()
     
     var TaskListResluts: Results<TaskModel>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        datePicker.datePickerMode = UIDatePicker.Mode.date
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.locale = Locale.current
+        scheduleEndAtTextField.inputView = datePicker
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35))
+        let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        toolbar.setItems([spacelItem, doneItem], animated: true)
+        
+        scheduleEndAtTextField.inputView = datePicker
+        scheduleEndAtTextField.inputAccessoryView = toolbar
+        
 
+    }
+    
+    @objc func done() {
+        scheduleEndAtTextField.endEditing(true)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+
+        scheduleEndAtTextField.text = "\(formatter.string(from: datePicker.date))"
     }
     
     @IBAction func send(_ sender: Any) {
